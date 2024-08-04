6104f26b0f62325a761744383ca538bccac05a54
Muhammad Abdullah <mafivs51@gmail.com>
2024-08-05 02:34:27 +0500

    some responsiveness fixes in experience sections

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import "./Experience.css"; // Import the CSS file for styling
 
-export default function Experience(
-  { data } // Pass the data prop to the Experience component
-) {
+export default function Experience({ data }) {
   return (
     <>
       <h2 className="mb-5">{data.title}</h2>
 
       {data.data.map((item, index) => (
-        <div className="container-fluid mt-4 ps-0">
-          <div className="row" key={index}>
+        <div className="container-fluid mt-4 ps-0" key={index}>
+          <div className="row">
             <div className="col-md-2">
               <a href={item.link}>
                 <div className="icon-container">
@@ -25,9 +23,9 @@ export default function Experience(
             </div>
             <div className="col-md-10">
               <div className="row mb-2">
-                <div className="col-md-12 p-0">
+                <div className="col-md-12">
                   <a href={item.link} className="d-flex align-items-center">
-                    <h4 className="mb-0">{item.company}</h4>
+                    <h4 className="mb-0 position-title">{item.company}</h4>
                     <img
                       width="20"
                       height="20"
@@ -39,17 +37,17 @@ export default function Experience(
                 </div>
               </div>
               <div className="row">
-                <div className="col-md-8 ps-0 pe-0">
-                  <h3>{item.title}</h3>
+                <div className="col-12 col-md-8">
+                  <h3 className="position-title">{item.title}</h3>
                 </div>
-                <div className="col-md-4 ps-0 pe-0 d-flex align-items-end">
-                  <h3>{item.date}</h3>
+                <div className="col-12 col-md-4 d-flex align-items-end">
+                  <h3 className="date">{item.date}</h3>
                 </div>
               </div>
               {/* only add this div if the item has a desc */}
               {item.desc && (
                 <div className="row">
-                  <div className="col-md-12 ps-0 pe-0 mt-4">
+                  <div className="col-md-12 mt-4">
                     <p>{item.desc}</p>
                   </div>
                 </div>
